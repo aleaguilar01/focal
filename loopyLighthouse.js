6be18519ab70e4c4cc331f4040e587bdc15beaef
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-02-25 21:30:58 -0600

    Adding create fizzBuzz function.

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -8,3 +8,14 @@ const createFizzBuzz = function(initialRange, finalRange, param1, param2, word1,
 };
 
 createFizzBuzz(1,100,3,5,"loopy","lighthouse");
+
+
+const createFizzBuzz2 = function(initialRange, finalRange, param1, param2, word1, word2) {
+  for (let i = initialRange; i <= finalRange; i++) {
+    console.log(
+      (i % param1 === 0 ? word1 : "") + (i % param2 === 0 ? word2 : "") || i
+    );
+  }
+};
+
+createFizzBuzz2(1,100,3,5,"loopy","lighthouse");
