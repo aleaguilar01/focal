2039f479e58dfb61c243c9d6b2505a83cfafac7e
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-02-25 19:28:55 -0600

    fixing obfuscate password file

--- password.js
+++ password.js
@@ -1,6 +1,6 @@
 const password = process.argv[2];
 
-const obfuscatePassword = function(password) {
+const obfuscate = function(password) {
   let finalPassword = "";
   for (let letter of password) {
     let character = letter;
@@ -23,31 +23,10 @@ const obfuscatePassword = function(password) {
   return finalPassword;
 };
 
-console.log(obfuscatePassword(password));
+console.log(obfuscate(password));
 
 
-console.log(obfuscatePassword("haneol"));
-
-
-const passwordMaker = function(password) {
-  const letterChanges = {
-    "a": "4",
-    "e": "3",
-    "o": "0",
-    "l": "1"
-  };
-  let finalPassword = " ";
-  for (let letter in password) {
-    if (letterChanges.values.includes(letter)) {
-      letter = letterChanges[letter];
-    }
-    finalPassword += letter;
-  }
-  return finalPassword;
-};
-
-
-console.log(passwordMaker("aeiol"));
+console.log(obfuscate("haneol"));
 
 //Every "a" in the string should be replaced with a "4".
 //Every "e" in the string should be replaced with a "3".
